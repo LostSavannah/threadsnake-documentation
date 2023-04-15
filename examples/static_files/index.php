1f38cb48fdc012ef679d8b5078641801b08d4003
codeRookieErick <erickfernandomoraramirez@gmail.com>
2023-04-14 20:57:15 -0400

    Setting static files example

--- examples/static_files/index.php
+++ examples/static_files/index.php
@@ -2,11 +2,29 @@
 <html lang="en">
 <?PHP require('../../parts/head.php'); ?>
 <body>
-    This is an example
+
+    <h1>
+        Static files
+    </h1>
+
+    <i> 
+        Static files in this scenario reffers to files that are served as-is,  
+        without any kind of modification or processing by the server.
+    </i>
+
+    The following example demostrates how to configure an application able to
+    serve static files:
 
     <pre><code class="python">
-    def functo():
-        pass
+import asyncio
+from threadsnake.turbo import Application
+from threadsnake.http.middlewares.app import static
+
+app:Application = Application(8089)
+
+app.configure(static('static'))
+
+asyncio.run(app.run())
     </code></pre>
 </body>
 </html>
--- parts/head.php
+++ parts/head.php
@@ -2,6 +2,7 @@
     <meta charset="utf-8">
     <title>Threadsnake</title>
     <link rel="stylesheet" href="https://dev.moradev.dev/threadsnake/styles/highlight.css">
+    <link rel="stylesheet" href="https://dev.moradev.dev/threadsnake/styles/main.css">
     <script src="https://dev.moradev.dev/threadsnake/scripts/highlight.js"></script>
     <script>hljs.highlightAll();</script>
 </head>
