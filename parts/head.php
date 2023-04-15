0d0a405d3e7a55c99f025acb727793c8b394fbd6
codeRookieErick <erickfernandomoraramirez@gmail.com>
2023-04-14 20:35:05 -0400

    Adding local highlight

--- parts/head.php
+++ parts/head.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="utf-8">
     <title>Threadsnake</title>
-    <link rel="stylesheet" href="styles/highlight.css">
-    <script src="scripts/highlight.js"></script>
+    <link rel="stylesheet" href="https://dev.moradev.dev/threadsnake/styles/highlight.css">
+    <script src="https://dev.moradev.dev/threadsnake/scripts/highlight.js"></script>
     <script>hljs.highlightAll();</script>
 </head>
